cd13cc4c9d5d29f49fd8fe03b018023e24149af7
Fusa-F <cist.b218.f.fukushi@gmail.com>
2021-09-06 10:11:47 +0900

    Initial Commit

---
+++ Shared/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Shared
+//
+//  Created by user on 2021/09/06.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
